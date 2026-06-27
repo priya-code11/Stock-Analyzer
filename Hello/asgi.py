1010c3af98174a1a17c523b1c34c996978fc8a2f
priya-code11 <priya.prajapati5578@gmail.com>
2026-06-27 11:14:49 +0530

    Fix folder name to underscore and update imports

--- Hello/asgi.py
+++ Hello/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from Home.routing import websocket_urlpatterns
+from stock_analyzer.routing import websocket_urlpatterns
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
