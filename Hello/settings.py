81d07cee7b2228223f97e28d70b430bb83a41ff8
priya-code11 <priya.prajapati5578@gmail.com>
2026-07-15 20:20:01 +0530

    fix folder rename error

--- Hello/settings.py
+++ Hello/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Home.context_processors.notification_context',
+                'stock_analyzer.context_processors.notification_context',
             ],
         },
     },
